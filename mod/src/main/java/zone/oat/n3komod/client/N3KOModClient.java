316577457478e04d8a941458dd60eb57ad17189d
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2022-11-09 23:22:22 +0300

    mod: buttons now have labels; also tinkered more with thread fog settings

--- mod/src/main/java/zone/oat/n3komod/client/N3KOModClient.java
+++ mod/src/main/java/zone/oat/n3komod/client/N3KOModClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
 import net.minecraft.client.render.RenderLayer;
 import zone.oat.n3komod.client.render.N3KOShaders;
 import zone.oat.n3komod.client.render.ThreadSkyRenderHandler;
+import zone.oat.n3komod.client.render.blockentity.ButtonBlockEntityRenderer;
 import zone.oat.n3komod.networking.N3KOS2CPackets;
 import zone.oat.n3komod.registry.N3KOBlockEntities;
 import zone.oat.n3komod.registry.N3KOBlocks;
@@ -19,6 +20,7 @@ public class N3KOModClient implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
     BlockEntityRendererRegistry.register(N3KOBlockEntities.PLUSH_BLOCK_ENTITY, PlushBlockEntityRenderer::new);
+    BlockEntityRendererRegistry.register(N3KOBlockEntities.BUTTON_BLOCK_ENTITY, ButtonBlockEntityRenderer::new);
     BlockRenderLayerMap.INSTANCE.putBlock(N3KOBlocks.SHE_PLUSH, RenderLayer.getTranslucent());
     BlockRenderLayerMap.INSTANCE.putBlock(N3KOBlocks.NOEL_PLUSH, RenderLayer.getTranslucent());
 
---
+++ mod/src/main/java/zone/oat/n3komod/client/render/blockentity/ButtonBlockEntityRenderer.java
@@ -0,0 +1,60 @@
+package zone.oat.n3komod.client.render.blockentity;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.WallSignBlock;
+import net.minecraft.block.enums.WallMountLocation;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3f;
+import zone.oat.n3komod.N3KOMod;
+import zone.oat.n3komod.content.block.ButtonBlock;
+import zone.oat.n3komod.content.blockentity.ButtonBlockEntity;
+import zone.oat.n3komod.content.blockentity.PlushBlockEntity;
+
+public class ButtonBlockEntityRenderer implements BlockEntityRenderer<ButtonBlockEntity> {
+  private final TextRenderer textRenderer;
+
+  public ButtonBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
+    this.textRenderer = ctx.getTextRenderer();
+  }
+
+  @Override
+  public void render(ButtonBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+    if (blockEntity.label.equals("")) return;
+
+    BlockState blockState = blockEntity.getCachedState();
+    OrderedText text = Text.of(blockEntity.label).asOrderedText();
+
+    matrices.push();
+
+    matrices.translate(0.5, 0.5, 0.5);
+    float h = -(blockState.get(ButtonBlock.FACING)).asRotation();
+    matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(h));
+    if (blockState.get(ButtonBlock.FACE).equals(WallMountLocation.FLOOR)) {
+      matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180f));
+      matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(-90f));
+    }
+    matrices.translate(0.0, 0.0, -0.162);
+    if (blockState.get(ButtonBlock.POWERED)) {
+      matrices.translate(0.0, 0.0, -0.06);
+    }
+    matrices.translate(0.0, 0.0F, 0.046666667F);
+    matrices.scale(0.010416667F, -0.010416667F, 0.010416667F);
+
+    float width = this.textRenderer.getWidth(text);
+    float height = this.textRenderer.fontHeight;
+    float size = 30f / width;
+    matrices.translate(0.0f, -(height * size) / 3, 0.0f);
+    matrices.scale(size, size, size);
+    this.textRenderer.drawWithOutline(text, -width / 2, 0, 0xFFFFFF, 0x111111, matrices.peek().getPositionMatrix(), vertexConsumers, light);
+
+    matrices.pop();
+  }
+}
